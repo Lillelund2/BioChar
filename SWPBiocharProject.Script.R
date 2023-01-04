6fea0f3b56a929e0e8fc9bdaadcd489cad5b3422
Lillelund2 <110987969+Lillelund2@users.noreply.github.com>
2023-01-04 12:42:22 +0000

    project file, script file and data file

---
+++ SWPBiocharProject.Script.R
@@ -0,0 +1,22 @@
+### Soil and Water Pollution - Experimental Assessment - Group Project ###
+### Biochar CO2 emissions and glyphosate breakdown capacity ###
+### Lucas Lillelund (Copenhagen University) tfv155@alumni.ku.dk ###
+
+
+library(agridat)
+library(ggthemes)
+library(lme4)
+library(ggplot2)
+library(dplyr)
+
+#set working directory
+getwd()
+
+#import data
+
+
+BioChar_Reps <- read.csv("Soil_Samps.csv", header = TRUE, sep = ";")
+mutate(Bio_Char)
+
+
+
